c026a4851190eb38264a112899e0bdfaf63c827d
Natã Ferreira Azevedo de Oliveira <nata.oliveira@zitrus.com.br>
2024-11-25 11:23:58 -0300

    Created project

---
+++ src/main/java/com/beatrizmed/dscatalago/DscatalagoApplication.java
@@ -0,0 +1,13 @@
+package com.beatrizmed.dscatalago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beatrizmed/dscatalago/DscatalagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beatrizmed.dscatalago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
